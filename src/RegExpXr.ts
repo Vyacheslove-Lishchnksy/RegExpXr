b2f6fdb5025d36d503269cf42af53e44f92b73a6
Lishchinsky Vyacheslave <lishchinsy@gmail.com>
2025-01-03 13:39:03 +0200

    Refactor RegExpXr class to rename 'regex' property to 'currentRegular'

--- src/RegExpXr.ts
+++ src/RegExpXr.ts
@@ -1,45 +1,55 @@
 export default class RegExpXr extends RegExp {
-  regex: string;
+  currentRegular: string;
 
   constructor(content: RegExp) {
     super(content.source, content.flags);
-    this.regex = (content.source === "(?:)") ? "" : content.source;
+    this.currentRegular = content.source === "(?:)" ? "" : content.source;
   }
 
   public gruop(content: RegExp) {
-    return new RegExpXr(new RegExp(`${this.regex}(${content.source})`));
+    return new RegExpXr(
+      new RegExp(`${this.currentRegular}(${content.source})`)
+    );
   }
 
   public diapasone(content: RegExp) {
-    return new RegExpXr(new RegExp(`${this.regex}[${content.source}]`));
+    return new RegExpXr(
+      new RegExp(`${this.currentRegular}[${content.source}]`)
+    );
   }
 
   public optional(content: RegExp) {
-    return new RegExpXr(new RegExp(`${this.regex}${content.source}?`));
+    return new RegExpXr(new RegExp(`${this.currentRegular}${content.source}?`));
   }
 
   public zeroOrMore(content: RegExp) {
-    return new RegExpXr(new RegExp(`${this.regex}${content.source ?? ""}*`));
+    return new RegExpXr(
+      new RegExp(`${this.currentRegular}${content.source ?? ""}*`)
+    );
   }
 
   public oneOrMore(content?: RegExp) {
-    return new RegExpXr(new RegExp(`${this.regex}${content?.source ?? ""}+`));
+    return new RegExpXr(
+      new RegExp(`${this.currentRegular}${content?.source ?? ""}+`)
+    );
   }
 
   public any() {
-    return new RegExpXr(new RegExp(`${this.regex}.`));
+    return new RegExpXr(new RegExp(`${this.currentRegular}.`));
   }
 
   public word(content: RegExp) {
-    return new RegExpXr(new RegExp(`${this.regex}\\b${content.source}\\b`))
+    return new RegExpXr(
+      new RegExp(`${this.currentRegular}\\b${content.source}\\b`)
+    );
   }
 
   public start() {
-    return new RegExpXr(new RegExp(`^${this.regex}`))
+    return new RegExpXr(new RegExp(`^${this.currentRegular}`));
   }
 
   public end() {
-    return new RegExpXr(new RegExp(`${this.regex}$`))
+    return new RegExpXr(new RegExp(`${this.currentRegular}$`));
   }
 
   public startWord() {
@@ -47,52 +57,58 @@ export default class RegExpXr extends RegExp {
   }
 
   public endWord() {
-    return new RegExpXr(new RegExp(`${this.regex}\\b`));
+    return new RegExpXr(new RegExp(`${this.currentRegular}\\b`));
   }
 
-  public diapasoneNumber({ from, to }: { from?: number, to?: number }) {
+  public diapasoneNumber({ from, to }: { from?: number; to?: number }) {
     if (from === to) {
-      return new RegExpXr(new RegExp(`${this.regex}{${from ?? ""}}`));
+      return new RegExpXr(new RegExp(`${this.currentRegular}{${from ?? ""}}`));
     }
-    return new RegExpXr(new RegExp(`${this.regex}{${from ?? ""},${to ?? ""}}`));
+    return new RegExpXr(
+      new RegExp(`${this.currentRegular}{${from ?? ""},${to ?? ""}}`)
+    );
   }
 
   public pickAfter(content: RegExp) {
-    return new RegExpXr(new RegExp(`${this.regex}(?<=${content.source})`));
+    return new RegExpXr(
+      new RegExp(`${this.currentRegular}(?<=${content.source})`)
+    );
   }
 
   public mustToBe(content: RegExp) {
-    return new RegExpXr(new RegExp(`${this.regex}(?=${content.source})`));
+    return new RegExpXr(
+      new RegExp(`${this.currentRegular}(?=${content.source})`)
+    );
   }
 
   public pickAfterNot(content: RegExp) {
-    return new RegExpXr(new RegExp(`${this.regex}(?<!${content.source})`));
+    return new RegExpXr(
+      new RegExp(`${this.currentRegular}(?<!${content.source})`)
+    );
   }
 
   public protected(content?: string) {
-    return new RegExpXr(new RegExp(`${this.regex}\\${content ?? ""}`));
+    return new RegExpXr(new RegExp(`${this.currentRegular}\\${content ?? ""}`));
   }
 
   public add(content: RegExp) {
-    return new RegExpXr(new RegExp(`${this.regex}${content.source}`));
+    return new RegExpXr(new RegExp(`${this.currentRegular}${content.source}`));
   }
 
   public or(content?: RegExp) {
-    return new RegExpXr(new RegExp(`${this.regex}|${content ?? ""}`));
+    return new RegExpXr(new RegExp(`${this.currentRegular}|${content ?? ""}`));
   }
 
-  public maskDiapasoneNumbers({ from, to }: { from: number, to: number }) {
+  public maskDiapasoneNumbers({ from, to }: { from: number; to: number }) {
     let regex: string = "";
 
     for (let i = from; i < to - 1; i++) {
       regex += `(${i})|`;
     }
 
-    regex += `(${to - 1})`
+    regex += `(${to - 1})`;
 
-    return new RegExpXr(new RegExp(
-      `(${regex})`
-    ));
+    return new RegExpXr(new RegExp(`${this.currentRegular}(${regex})`));
   }
 }
 
